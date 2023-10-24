8b5cd33d3dc5ea79feae72fa721f8206c9a077e6
saidame <said.amchghal@gmail.com>
2023-10-24 05:14:32 -0700

    m

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        - side_length: The length of each side of the square.
+    """
+    pass
